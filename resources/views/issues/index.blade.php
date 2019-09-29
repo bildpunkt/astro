3239951453fec10538d1f53e67dd74342b1b579c
Andreas Nedbal <git@pixelde.su>
2019-09-29 21:48:23 +0200

    :lipstick: improve design of issue listing

--- resources/views/issues/index.blade.php
+++ resources/views/issues/index.blade.php
@@ -2,31 +2,28 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                @include('projects.navigation')
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <a href="{{ route('issues.new', $project) }}" class="btn btn-primary">New Issue</a>
-                </div>
-                <ul class="list-group list-group-flush">
-                    @foreach ($project->issues as $issue)
-                        <li class="list-group-item">
-                            <a href="{{ route('issues.show', [$project, $issue]) }}">
-                                {{ $issue->subject }}
-                            </a>
-                        </li>
-                    @endforeach
-                </ul>
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
+    <div class="card">
+        @include('projects.navigation')
+        <div class="card-body">
+            <div class="d-flex w-100 align-items-center justify-content-between">
+                <h1>Issues</h1>
+                <a href="{{ route('issues.new', $project) }}" class="btn btn-primary">New Issue</a>
             </div>
         </div>
+        <ul class="list-group list-group-flush">
+            @foreach ($project->issues as $issue)
+                <li class="list-group-item">
+                    <a href="{{ route('issues.show', [$project, $issue]) }}">
+                        {{ $issue->subject }}
+                    </a>
+                </li>
+            @endforeach
+        </ul>
     </div>
 </div>
 @endsection
