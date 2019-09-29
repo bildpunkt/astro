b510fb8d0c3011f43f2e53c99fa09e0c59ecc502
Andreas Nedbal <git@pixelde.su>
2019-09-29 21:23:30 +0200

    :lipstick: improve design of project listing

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,35 +2,32 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Projects</div>
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <a href="{{ route('projects.new') }}" class="btn btn-primary">New Project</a>
-                </div>
-                <ul class="list-group list-group-flush">
-                    @foreach ($projects as $project)
-                        <li class="list-group-item">
-                            <h5>
-                                <a href="{{ route('projects.show', $project) }}">
-                                    {{ $project->name }}
-                                </a>
-                            </h5>
-                            @if ($project->description)
-                                <p>{{ $project->description }}</p>
-                            @endif
-                        </li>
-                    @endforeach
-                </ul>
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
+    <div class="card">
+        <div class="card-body">
+            <div class="d-flex w-100 align-items-center justify-content-between">
+                <h1>Projects</h1>
+                <a href="{{ route('projects.new') }}" class="btn btn-primary">New Project</a>
             </div>
         </div>
+        <ul class="list-group list-group-flush">
+            @foreach ($projects as $project)
+                <li class="list-group-item">
+                    <h5>
+                        <a href="{{ route('projects.show', $project) }}">
+                            {{ $project->name }}
+                        </a>
+                    </h5>
+                    @if ($project->description)
+                        <p>{{ $project->description }}</p>
+                    @endif
+                </li>
+            @endforeach
+        </ul>
     </div>
 </div>
 @endsection
