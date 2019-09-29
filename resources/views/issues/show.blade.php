39137ab3ce8de430220e3aa9639e6832f327e72a
Andreas Nedbal <git@pixelde.su>
2019-09-29 21:55:45 +0200

    :lipstick: improve design of issue page

--- resources/views/issues/show.blade.php
+++ resources/views/issues/show.blade.php
@@ -2,41 +2,39 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ $issue->subject }}</div>
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <p>Opened by {{ $issue->author->name }}</p>
-
-                    @if($issue->assignee)
-                        <p>
-                            Assigned to {{ $issue->assignee->name }}
-                        </p>
-                    @endif
-
-                    @if($issue->milestone)
-                        <p>
-                            Milestone:
-                            <a href="{{ route('milestones.show', [$issue->milestone->project, $issue->milestone]) }}">
-                                {{ $issue->milestone->name }}
-                            </a>
-                        </p>
-                    @endif
-
-                    @if($issue->description)
-                        <p>{{ $issue->description }}</p>
-                    @endif
-
-                    <a href="{{ route('issues.edit', [$issue->project, $issue]) }}" class="btn btn-primary">Edit Issue</a>
-                </div>
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
+    <div class="card">
+        @include('projects.navigation', ['project' => $issue->project])
+        <div class="card-body">
+            <div class="d-flex w-100 align-items-center justify-content-between">
+                <h1>{{ $issue->subject }}</h1>
+                <a href="{{ route('issues.edit', [$issue->project, $issue]) }}" class="btn btn-outline-secondary">Edit Issue</a>
             </div>
+
+            <p>Opened by {{ $issue->author->name }}</p>
+
+            @if($issue->assignee)
+                <p>
+                    Assigned to {{ $issue->assignee->name }}
+                </p>
+            @endif
+
+            @if($issue->milestone)
+                <p>
+                    Milestone:
+                    <a href="{{ route('milestones.show', [$issue->milestone->project, $issue->milestone]) }}">
+                        {{ $issue->milestone->name }}
+                    </a>
+                </p>
+            @endif
+
+            @if($issue->description)
+                <p>{{ $issue->description }}</p>
+            @endif
         </div>
     </div>
 </div>
