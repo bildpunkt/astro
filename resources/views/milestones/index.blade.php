3022d31f4782dc5d5e123fefebf73d7bd236fe5d
Andreas Nedbal <git@pixelde.su>
2019-09-29 21:37:20 +0200

    :lipstick: improve design of milestone listing

--- resources/views/milestones/index.blade.php
+++ resources/views/milestones/index.blade.php
@@ -2,34 +2,31 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                @include('projects.navigation')
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <a href="{{ route('milestones.new', $project) }}" class="btn btn-primary">New Milestone</a>
-                </div>
-                <ul class="list-group list-group-flush">
-                    @foreach ($project->milestones as $milestone)
-                        <li class="list-group-item">
-                            <a href="{{ route('milestones.show', [$project, $milestone]) }}">
-                                {{ $milestone->name }}
-                            </a>
-                            @if ($milestone->description)
-                                <p>{{ $milestone->description }}</p>
-                            @endif
-                        </li>
-                    @endforeach
-                </ul>
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
+    <div class="card">
+        @include('projects.navigation')
+        <div class="card-body">
+            <div class="d-flex w-100 align-items-center justify-content-between">
+                <h1>Milestones</h1>
+                <a href="{{ route('milestones.new', $project) }}" class="btn btn-primary">New Milestone</a>
             </div>
         </div>
+        <ul class="list-group list-group-flush">
+            @foreach ($project->milestones as $milestone)
+                <li class="list-group-item">
+                    <a href="{{ route('milestones.show', [$project, $milestone]) }}">
+                        {{ $milestone->name }}
+                    </a>
+                    @if ($milestone->description)
+                        <p>{{ $milestone->description }}</p>
+                    @endif
+                </li>
+            @endforeach
+        </ul>
     </div>
 </div>
 @endsection
