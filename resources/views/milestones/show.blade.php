93f8436bb902285da19fbf439790d3263f946446
Andreas Nedbal <git@pixelde.su>
2019-09-29 21:44:50 +0200

    :lipstick: improve design of milestone page

--- resources/views/milestones/show.blade.php
+++ resources/views/milestones/show.blade.php
@@ -2,36 +2,34 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ $milestone->name }}</div>
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    @if($milestone->description)
-                        <p>{{ $milestone->description }}</p>
-                    @endif
-
-                    @if($milestone->issues)
-                        <ul class="list-group">
-                            @foreach ($milestone->issues as $issue)
-                                <li class="list-group-item">
-                                    <a href="{{ route('issues.show', [$issue->project, $issue]) }}">
-                                        {{ $issue->subject }}
-                                    </a>
-                                </li>
-                            @endforeach
-                        </ul>
-                    @endif
-
-                    <a href="{{ route('milestones.edit', [$milestone->project, $milestone]) }}" class="btn btn-primary">Edit Milestone</a>
-                </div>
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
+    <div class="card">
+        @include('projects.navigation', ['project' => $milestone->project])
+        <div class="card-body">
+            <div class="d-flex w-100 align-items-center justify-content-between">
+                <h1>{{ $milestone->name }}</h1>
+                <a href="{{ route('milestones.edit', [$milestone->project, $milestone]) }}" class="btn btn-outline-secondary">Edit Milestone</a>
             </div>
+
+            @if($milestone->description)
+                <p>{{ $milestone->description }}</p>
+            @endif
+
+            @if($milestone->issues)
+                <ul class="list-group">
+                    @foreach ($milestone->issues as $issue)
+                        <li class="list-group-item">
+                            <a href="{{ route('issues.show', [$issue->project, $issue]) }}">
+                                {{ $issue->subject }}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            @endif
         </div>
     </div>
 </div>
