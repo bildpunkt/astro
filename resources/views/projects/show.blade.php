917bc92e48dcb3ca691c3c5887a8beddfa347de7
Andreas Nedbal <git@pixelde.su>
2019-09-29 21:32:06 +0200

    :lipstick: improve design of project page

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -2,35 +2,29 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                @include('projects.navigation')
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
+    <div class="card">
+        @include('projects.navigation')
 
-                <div class="card-body">
-                    <h1>
-                        {{ $project->name }}
-                    </h1>
-
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    @if($project->description)
-                        <p>{{ $project->description }}</p>
-                    @endif
-
-                    @if($project->url)
-                        <p>
-                            <a href="{{ $project->url }}">{{ $project->url }}</a>
-                        </p>
-                    @endif
-
-                    <a href="{{ route('projects.edit', $project) }}" class="btn btn-primary">Edit Project</a>
-                </div>
+        <div class="card-body">
+            <div class="d-flex w-100 align-items-center justify-content-between">
+                <h1>{{ $project->name }}</h1>
+                <a href="{{ route('projects.edit', $project) }}" class="btn btn-outline-secondary">Edit Project</a>
             </div>
+
+            @if($project->description)
+                <p>{{ $project->description }}</p>
+            @endif
+
+            @if($project->url)
+                <p>
+                    <a href="{{ $project->url }}">{{ $project->url }}</a>
+                </p>
+            @endif
         </div>
     </div>
 </div>
