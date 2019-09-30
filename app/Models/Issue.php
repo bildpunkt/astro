37b5fbc27cc6d8f24cb7df2d6c1243bb155ed99f
Andreas Nedbal <git@pixelde.su>
2019-09-30 22:07:19 +0200

    :pencil: document Issue model

--- app/Models/Issue.php
+++ app/Models/Issue.php
@@ -8,21 +8,41 @@ class Issue extends Model
 {
     protected $fillable = ['subject', 'description', 'assigned_to_id', 'author_id', 'project_id', 'milestone_id'];
 
+    /**
+     * Author of the issue
+     *
+     * @return \App\Models\User
+     */
     public function author()
     {
         return $this->belongsTo('App\Models\User', 'author_id');
     }
 
+    /**
+     * User assigned to the issue
+     *
+     * @return \App\Models\User
+     */
     public function assignee()
     {
         return $this->belongsTo('App\Models\User', 'assigned_to_id');
     }
 
+    /**
+     * Milestone the issue belongs to
+     *
+     * @return \App\Models\Milestone
+     */
     public function milestone()
     {
         return $this->belongsTo('App\Models\Milestone');
     }
 
+    /**
+     * Project the issue belongs to
+     *
+     * @return \App\Models\Project
+     */
     public function project()
     {
         return $this->belongsTo('App\Models\Project');
