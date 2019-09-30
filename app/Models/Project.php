4dc4cbdabe0480253ccf4ef3715e49865d1d8827
Andreas Nedbal <git@pixelde.su>
2019-09-30 22:07:53 +0200

    :pencil: document Project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,11 +8,21 @@ class Project extends Model
 {
     protected $fillable = ['name', 'description', 'url'];
 
+    /**
+     * Issues that are part of a project
+     *
+     * @return \Illuminate\Database\Eloquent\Collection<\App\Models\Issue>
+     */
     public function issues()
     {
         return $this->hasMany('App\Models\Issue');
     }
 
+    /**
+     * Milestones that are part of a project
+     *
+     * @return \Illuminate\Database\Eloquent\Collection<\App\Models\Milestone>
+     */
     public function milestones()
     {
         return $this->hasMany('App\Models\Milestone');
