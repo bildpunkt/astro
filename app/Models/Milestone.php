e512923afe978fe12f8153192f925102a633b367
Andreas Nedbal <git@pixelde.su>
2019-09-30 22:07:41 +0200

    :pencil: document Milestone model

--- app/Models/Milestone.php
+++ app/Models/Milestone.php
@@ -8,11 +8,21 @@ class Milestone extends Model
 {
     protected $fillable = ['name', 'description', 'project_id'];
 
+    /**
+     * Issues that are part of a milestone
+     *
+     * @return \Illuminate\Database\Eloquent\Collection<\App\Models\Issue>
+     */
     public function issues()
     {
         return $this->hasMany('App\Models\Issue');
     }
 
+    /**
+     * Project the milestone belongs to
+     *
+     * @return \App\Models\Project
+     */
     public function project()
     {
         return $this->belongsTo('App\Models\Project');
