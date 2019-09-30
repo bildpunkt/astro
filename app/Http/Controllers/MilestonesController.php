a4273e1a121033c102574dccfcdee1d75fe33ec8
Andreas Nedbal <git@pixelde.su>
2019-09-30 21:03:05 +0200

    :pencil: document MilestonesController

--- app/Http/Controllers/MilestonesController.php
+++ app/Http/Controllers/MilestonesController.php
@@ -8,21 +8,48 @@ use App\Models\Milestone;
 
 class MilestonesController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
     public function __construct()
     {
         $this->middleware('auth');
     }
 
+    /**
+     * Show a projects milestone listing
+     *
+     * @param \App\Models\Project
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
     public function index(Project $project)
     {
         return view('milestones.index', ['project' => $project]);
     }
 
+    /**
+     * Show the page to create a new milestone
+     *
+     * @param \App\Models\Project
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
     public function new(Project $project)
     {
         return view('milestones.new', ['project' => $project]);
     }
 
+    /**
+     * POST-action that creates a new milestone
+     *
+     * @param \Illuminate\Http\Request
+     * @param \App\Models\Project
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function create(Request $request, Project $project)
     {
         $validatedData = $request->validate([
@@ -37,11 +64,28 @@ class MilestonesController extends Controller
         return redirect()->route('milestones.show', [$project->id, $milestone->id])->with('status', 'Milestone successfully created!');
     }
 
+    /**
+     * Show the page to edit a milestone
+     *
+     * @param \App\Models\Project
+     * @param \App\Models\Milestone
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
     public function edit(Project $project, Milestone $milestone)
     {
         return view('milestones.edit', ['milestone' => $milestone]);
     }
 
+    /**
+     * PUT-action to update a milestones properties
+     *
+     * @param \Illuminate\Http\Request
+     * @param \App\Models\Project
+     * @param \App\Models\Milestone
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function update(Request $request, Project $project, Milestone $milestone)
     {
         $validatedData = $request->validate([
@@ -54,11 +98,27 @@ class MilestonesController extends Controller
         return redirect()->route('milestones.show', ['project' => $project, 'milestone' => $milestone])->with('status', 'Milestone successfully updated!');
     }
 
+    /**
+     * Show the detail page of a milestone
+     *
+     * @param \App\Models\Project
+     * @param \App\Models\Milestone
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
     public function show(Project $project, Milestone $milestone)
     {
         return view('milestones.show', ['milestone' => $milestone]);
     }
 
+    /**
+     * DELETE-action to delete a milestone
+     *
+     * @param \App\Models\Project
+     * @param \App\Models\Milestone
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function destroy(Project $project, Milestone $milestone)
     {
         $milestone->delete();
