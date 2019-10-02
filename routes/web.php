8b84c56b109da5758a3be83b80558fc24ae7c05f
Andreas Nedbal <git@pixelde.su>
2019-10-02 19:54:57 +0200

    :recycle: group routes

--- routes/web.php
+++ routes/web.php
@@ -21,26 +21,32 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/admin', 'AdminController@index')->name('admin.index');
 
-Route::get('/projects', 'ProjectsController@index')->name('projects.index');
-Route::get('/projects/new', 'ProjectsController@new')->name('projects.new');
-Route::post('/projects/create', 'ProjectsController@create')->name('projects.create');
-Route::get('/projects/{project}', 'ProjectsController@show')->name('projects.show');
-Route::get('/projects/{project}/edit', 'ProjectsController@edit')->name('projects.edit');
-Route::put('/projects/{project}/update', 'ProjectsController@update')->name('projects.update');
-Route::delete('/projects/{project}/destroy', 'ProjectsController@destroy')->name('projects.destroy');
+Route::prefix('projects')->group(function () {
+    Route::get('/', 'ProjectsController@index')->name('projects.index');
+    Route::get('/new', 'ProjectsController@new')->name('projects.new');
+    Route::post('/create', 'ProjectsController@create')->name('projects.create');
+    Route::get('/{project}', 'ProjectsController@show')->name('projects.show');
+    Route::get('/{project}/edit', 'ProjectsController@edit')->name('projects.edit');
+    Route::put('/{project}/update', 'ProjectsController@update')->name('projects.update');
+    Route::delete('/{project}/destroy', 'ProjectsController@destroy')->name('projects.destroy');
 
-Route::get('/projects/{project}/issues', 'IssuesController@index')->name('issues.index');
-Route::get('/projects/{project}/issues/new', 'IssuesController@new')->name('issues.new');
-Route::post('/projects/{project}/issues/create', 'IssuesController@create')->name('issues.create');
-Route::get('/projects/{project}/issues/{issue}', 'IssuesController@show')->name('issues.show');
-Route::get('/projects/{project}/issues/{issue}/edit', 'IssuesController@edit')->name('issues.edit');
-Route::put('/projects/{project}/issues/{issue}/update', 'IssuesController@update')->name('issues.update');
-Route::delete('/projects/{project}/issues/{issue}/destroy', 'IssuesController@destroy')->name('issues.destroy');
+    Route::prefix('/{project}/issues')->group(function () {
+        Route::get('/', 'IssuesController@index')->name('issues.index');
+        Route::get('/new', 'IssuesController@new')->name('issues.new');
+        Route::post('/create', 'IssuesController@create')->name('issues.create');
+        Route::get('/{issue}', 'IssuesController@show')->name('issues.show');
+        Route::get('/{issue}/edit', 'IssuesController@edit')->name('issues.edit');
+        Route::put('/{issue}/update', 'IssuesController@update')->name('issues.update');
+        Route::delete('/{issue}/destroy', 'IssuesController@destroy')->name('issues.destroy');
+    });
 
-Route::get('/projects/{project}/milestones', 'MilestonesController@index')->name('milestones.index');
-Route::get('/projects/{project}/milestones/new', 'MilestonesController@new')->name('milestones.new');
-Route::post('/projects/{project}/milestones/create', 'MilestonesController@create')->name('milestones.create');
-Route::get('/projects/{project}/milestones/{milestone}', 'MilestonesController@show')->name('milestones.show');
-Route::get('/projects/{project}/milestones/{milestone}/edit', 'MilestonesController@edit')->name('milestones.edit');
-Route::put('/projects/{project}/milestones/{milestone}/update', 'MilestonesController@update')->name('milestones.update');
-Route::delete('/projects/{project}/milestones/{milestone}/destroy', 'MilestonesController@destroy')->name('milestones.destroy');
+    Route::prefix('/{project}/milestones')->group(function () {
+        Route::get('/', 'MilestonesController@index')->name('milestones.index');
+        Route::get('/new', 'MilestonesController@new')->name('milestones.new');
+        Route::post('/create', 'MilestonesController@create')->name('milestones.create');
+        Route::get('/{milestone}', 'MilestonesController@show')->name('milestones.show');
+        Route::get('/{milestone}/edit', 'MilestonesController@edit')->name('milestones.edit');
+        Route::put('/{milestone}/update', 'MilestonesController@update')->name('milestones.update');
+        Route::delete('/{milestone}/destroy', 'MilestonesController@destroy')->name('milestones.destroy');
+    });
+});
