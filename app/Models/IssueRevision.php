37906a251dc865466a3a2bc66772dcd39b9d338d
Andreas Nedbal <git@pixelde.su>
2019-10-03 11:28:47 +0200

    :sparkles: transform IssueRevision attributes to model representations

--- app/Models/IssueRevision.php
+++ app/Models/IssueRevision.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\Milestone;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class IssueRevision extends Model
@@ -35,6 +37,30 @@ class IssueRevision extends Model
      */
     public function getAttributesAttribute($value)
     {
-        return json_decode($value);
+        $attributes = json_decode($value, true);
+
+        foreach ($attributes as $attribute => $values) {
+            switch ($attribute) {
+                case 'assigned_to_id':
+                    $attributes['assignee'] = [
+                        'old' => User::find($values['old']),
+                        'new' => User::find($values['new'])
+                    ];
+
+                    unset($attributes['assigned_to_id']);
+                    break;
+                case 'milestone_id':
+                    $attributes['milestone'] = [
+                        'old' => Milestone::find($values['old']),
+                        'new' => Milestone::find($values['new'])
+                    ];
+
+                    unset($attributes['milestone_id']);
+                    break;
+                default:
+            }
+        }
+
+        return $attributes;
     }
 }
