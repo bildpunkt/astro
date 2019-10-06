93bf5ddc4906009afd74e87f3aeb066f5acdeceb
Andreas Nedbal <git@pixelde.su>
2019-10-06 23:34:52 +0200

    :sparkles: add IssueCategory model

---
+++ database/migrations/2019_10_06_212929_create_issue_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIssueCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('issue_categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table
+                ->string('name')
+                ->default('')
+                ->nullable(false);
+            $table
+                ->text('description')
+                ->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('issue_categories');
+    }
+}
