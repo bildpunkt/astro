746fc67721dcea00642634528ba74244d4d43217
Andreas Nedbal <andy@pixelde.su>
2019-11-01 01:11:29 +0100

    :globe_with_meridians: add admin locales

---
+++ resources/lang/en/admin.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'title' => 'Administration',
+    'manage' => 'Manage',
+    'issues' => [
+        'title' => 'Issues',
+        'categories' => [
+            'title' => 'Issue Categories',
+            'subtitle' => 'Categories that can be assigned to issues.'
+        ],
+        'priorities' => [
+            'title' => 'Issue Priorities',
+            'subtitle' => 'Priorities that can be assigned to issues.'
+        ],
+        'types' => [
+            'title' => 'Issue Types',
+            'subtitle' => 'Types of issues that can be created.'
+        ]
+    ]
+];
